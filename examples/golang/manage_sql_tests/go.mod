f0375647d17071e2979c5f2b6cd64c21df73f09c
Synqlair <ci@synq.io>
2024-06-26 15:22:37 +0000

    Publish public protos

---
+++ examples/golang/manage_sql_tests/go.mod
@@ -0,0 +1,20 @@
+module github.com/getsynq/api/examples/go/sql_tests
+
+go 1.21.6
+
+require (
+	buf.build/gen/go/getsynq/api/grpc/go v1.4.0-20240626145232-ca580549697c.1
+	buf.build/gen/go/getsynq/api/protocolbuffers/go v1.34.2-20240626145232-ca580549697c.2
+	golang.org/x/oauth2 v0.20.0
+	google.golang.org/grpc v1.64.0
+)
+
+require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.2-20240508200655-46a4cf4ba109.2 // indirect
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ examples/golang/manage_sql_tests/main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+
+	"golang.org/x/oauth2/clientcredentials"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/oauth"
+
+	sqltestsv1grpc "buf.build/gen/go/getsynq/api/grpc/go/synq/datachecks/sqltests/v1/sqltestsv1grpc"
+	sqltestsv1 "buf.build/gen/go/getsynq/api/protocolbuffers/go/synq/datachecks/sqltests/v1"
+	entitiesv1 "buf.build/gen/go/getsynq/api/protocolbuffers/go/synq/entities/v1"
+	platformsv1 "buf.build/gen/go/getsynq/api/protocolbuffers/go/synq/platforms/v1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	host := "developer.synq.dev"
+	port := "443"
+	apiUrl := fmt.Sprintf("%s:%s", host, port)
+
+	clientID := "foo"
+	clientSecret := "bar"
+	tokenURL := fmt.Sprintf("https://%s/oauth2/token", host)
+
+	config := &clientcredentials.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		TokenURL:     tokenURL,
+	}
+	oauthTokenSource := oauth.TokenSource{TokenSource: config.TokenSource(ctx)}
+	creds := credentials.NewTLS(&tls.Config{InsecureSkipVerify: false})
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(creds),
+		grpc.WithPerRPCCredentials(oauthTokenSource),
+		grpc.WithAuthority(host),
+	}
+
+	conn, err := grpc.DialContext(ctx, apiUrl, opts...)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Printf("Connected to API...\n\n")
+
+	sqltestapi := sqltestsv1grpc.NewSqlTestsServiceClient(conn)
+
+	// Create sql tests.
+	{
+		upsertRes, err := sqltestapi.BatchUpsertSqlTests(ctx, &sqltestsv1.BatchUpsertSqlTestsRequest{
+			SqlTests: []*sqltestsv1.SqlTest{
+				{
+					Platform: &platformsv1.DataPlatformIdentifier{
+						Id: &platformsv1.DataPlatformIdentifier_Clickhouse{
+							Clickhouse: &platformsv1.ClickhouseIdentifier{
+								Host:   "q920vu7fv8.eu-west-1.aws.clickhouse.cloud",
+								Schema: "prod",
+							},
+						},
+					},
+					Id:             "/clickhouse/prod/check-created-at-not-null",
+					Name:           "CH/alertsv2 message_id not null",
+					SqlExpression:  "select * from alerts.alert_settings where workspace is null",
+					RecurrenceRule: "FREQ=HOURLY;INTERVAL=3",
+					SaveFailures:   true,
+					Annotations: []*entitiesv1.Annotation{
+						{
+							Name:   "env",
+							Values: []string{"prod"},
+						},
+						{
+							Name:   "owners",
+							Values: []string{"engineering", "karan"},
+						},
+					},
+				},
+				{
+					Platform: &platformsv1.DataPlatformIdentifier{
+						Id: &platformsv1.DataPlatformIdentifier_Postgres{
+							Postgres: &platformsv1.PostgresIdentifier{
+								Host:     "34.79.228.42",
+								Database: "kernel-alertsv2",
+							},
+						},
+					},
+					Id:             "/postgres/prod/alerts/mutes/created-at-not-null",
+					Name:           "Postgres/alerts mutes created_at not null",
+					SqlExpression:  "select * from public.mutes where created_at is null",
+					RecurrenceRule: "FREQ=HOURLY;INTERVAL=3",
+					SaveFailures:   true,
+					Annotations: []*entitiesv1.Annotation{
+						{
+							Name:   "env",
+							Values: []string{"prod"},
+						},
+						{
+							Name:   "owners",
+							Values: []string{"petr"},
+						},
+					},
+				},
+			},
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		if len(upsertRes.Errors) > 0 {
+			panic(fmt.Errorf("%s -> %v", upsertRes.Errors[0].Id, upsertRes.Errors[0].Reason))
+		}
+
+		if len(upsertRes.CreatedIds)+len(upsertRes.UpdatedIds) != 2 {
+			panic("all tests were not created")
+		}
+
+		fmt.Printf("Created tests: %+v\n\n", append(upsertRes.CreatedIds, upsertRes.GetUpdatedIds()...))
+	}
+
+	// Update sql tests by IDs.
+	{
+		upsertRes, err := sqltestapi.BatchUpsertSqlTests(ctx, &sqltestsv1.BatchUpsertSqlTestsRequest{
+			SqlTests: []*sqltestsv1.SqlTest{
+				{
+					Platform: &platformsv1.DataPlatformIdentifier{
+						Id: &platformsv1.DataPlatformIdentifier_Clickhouse{
+							Clickhouse: &platformsv1.ClickhouseIdentifier{
+								Host:   "q920vu7fv8.eu-west-1.aws.clickhouse.cloud",
+								Schema: "prod",
+							},
+						},
+					},
+					Id:             "/clickhouse/prod/check-created-at-not-null",
+					Name:           "CH/alertsv2 message_id not null",
+					SqlExpression:  "select * from alerts.alert_settings where workspace is null",
+					RecurrenceRule: "FREQ=HOURLY;INTERVAL=6",
+					SaveFailures:   true,
+					Annotations: []*entitiesv1.Annotation{
+						{
+							Name:   "env",
+							Values: []string{"prod"},
+						},
+						{
+							Name:   "owners",
+							Values: []string{"engineering", "karan"},
+						},
+					},
+				},
+			},
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		if len(upsertRes.Errors) > 0 {
+			panic(upsertRes.Errors[0].Reason)
+		}
+
+		if len(upsertRes.UpdatedIds) != 1 {
+			panic("all tests were not updated")
+		}
+
+		fmt.Printf("Updated tests: %+v\n\n", upsertRes.UpdatedIds)
+	}
+
+	// List sql tests by annotations.
+	{
+		listResp, err := sqltestapi.ListSqlTests(ctx, &sqltestsv1.ListSqlTestsRequest{
+			Annotations: []*entitiesv1.Annotation{
+				{Name: "env", Values: []string{"prod"}},
+			},
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		mappedTests := make(map[string]*sqltestsv1.SqlTest)
+		for _, test := range listResp.SqlTests {
+			mappedTests[test.Id] = test
+		}
+
+		if mappedTests["/clickhouse/prod/check-created-at-not-null"] == nil {
+			panic("missing test /clickhouse/prod/check-created-at-not-null in list response")
+		}
+
+		if mappedTests["/postgres/prod/alerts/mutes/created-at-not-null"] == nil {
+			panic("missing test /postgres/prod/alerts/mutes/created-at-not-null in list response")
+		}
+
+		fmt.Println("Fetched tests by annotation:")
+		for id, test := range mappedTests {
+			fmt.Printf("Test %s -> %s\n", id, test.String())
+		}
+		fmt.Println()
+	}
+
+	// Delete sql tests by IDs.
+	{
+		_, err := sqltestapi.BatchDeleteSqlTests(ctx, &sqltestsv1.BatchDeleteSqlTestsRequest{
+			Ids: []string{"/postgres/prod/alerts/mutes/created-at-not-null", "/clickhouse/prod/check-created-at-not-null"},
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Deleted tests.")
+	}
+
+}
