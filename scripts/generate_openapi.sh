321eb021e236b103117b1cdec0ec095961cb6f7f
Synqlair <ci@synq.io>
2025-07-23 11:42:17 +0000

    Publish public protos

--- scripts/generate_openapi.sh
+++ scripts/generate_openapi.sh
@@ -71,5 +71,28 @@ protoc \
     --connect-openapi_opt=content-types=json \
     $(grep -r -l "google.api.http" ${PROTOS_DIR}/synq)
 
+# Having an explicit title on any of the schemas that use `oneOf` makes the mintlify docs hard to read
+# since it uses that title for each variant, even though the variants all have explicit titles as well.
+# As a workaround for that, we remove the title property from any schema object that has `oneOf`.
+#
+# Relevant issue: https://linear.app/synq/issue/PR-5531/mintlify-docs-renders-oneof-variants-poorly-each-variant-has-the-same
+yq \
+  --inplace \
+  --indent=4 \
+  --input-format=yaml \
+  --output-format=yaml \
+  '
+    del(.components.schemas.[] | select (.oneOf != null and .title != null).title) |
+    
+    # Similarly, we delete the title from any properties that use a reference,
+    # as that also overrides the title used by Mintlify.
+    del(.components.schemas.[] | select (.properties != null).properties.[] | select (".$ref" != null and .title != null).title) |
+
+    # Lastly, we need to do the same for any paths that specify these titles explicitly
+    del(.. | select(has("schema")).schema | select(has("properties")).properties.[] | select(has("$ref") and has("title")).title)
+    ' \
+  "${DOCS_DIR}/openapi.yaml"
+
+
 set +e
 exit 0
